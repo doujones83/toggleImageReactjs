39e7f1120d8393e1f7f4ba710413c3c7a01aa62e
William Boswell <doujones@Williams-MacBook-Air.local>
2021-10-11 07:26:18 -0400

    commit image

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
-  return (
+
+    const App = () => {
+         const url = "https://images.unsplash.com/photo-1562051036-e0eea191d42f";
+         const [ displayImage, setState ] = useState(false);
+         const toggleImage = () => setState(!displayImage);
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {displayImage && <img src={url} alt="Some coffee beans" />}
+      <br />
+      <button onClick={toggleImage}>Toggle Image Display</button>
     </div>
-  );
-}
-
+    );
+  };
 export default App;
